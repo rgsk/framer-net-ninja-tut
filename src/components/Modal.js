eca53e968969fe2a785b9fb5f1494fdada402d34
rgsk <guptarahul70322@gmail.com>
2021-10-13 00:09:11 +0530

    first commit

---
+++ src/components/Modal.js
@@ -0,0 +1,55 @@
+import { AnimatePresence, motion } from "framer-motion";
+import React from "react";
+import { Link } from "react-router-dom";
+const backdropVariants = {
+  visible: {
+    opacity: 1,
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+const modalVariants = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "200px",
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+    },
+  },
+};
+function Modal({ showModal, setShowModal }) {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {showModal && (
+        <motion.div
+          className="backdrop"
+          variants={backdropVariants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+        >
+          <motion.div className="modal" variants={modalVariants}>
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <button
+                onClick={() => {
+                  // setShowModal(false);
+                  // not needed
+                }}
+              >
+                Start Again
+              </button>
+            </Link>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+}
+
+export default Modal;
